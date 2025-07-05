b4b2a97c4a99996ec12f2a58ef44517175dc6dad
glyphszyme <47078648+glyphszyme@users.noreply.github.com>
2025-07-06 06:43:42 +0900

    alternate Interface

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,29 +2,25 @@ import { Suspense } from "react";
 import MovieInfo, { getInfo } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-    params: {
-        id: string;
-    };
-}
+export const dynamic = "force-dynamic"; // (필요시 추가)
 
-export async function generateMetadata(props: IParams) {
+export async function generateMetadata(props: { params: { id: string } }) {
     const { id } = props.params;
     const movie = await getInfo(id);
     return {
         title: `${movie.title} | Next Movies`,
-    }
+    };
 }
 
-export default async function MovieDetail(props: IParams) {
+export default async function MovieDetail(props: { params: { id: string } }) {
     const { id } = props.params;
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info...</h1>}>
-                <MovieInfo id={id}/>
+                <MovieInfo id={id} />
             </Suspense>
-            <Suspense fallback={<h1>Loading movie vidoes...</h1>}>
-                <MovieVideos id={id}/>
+            <Suspense fallback={<h1>Loading movie videos...</h1>}>
+                <MovieVideos id={id} />
             </Suspense>
         </div>
     );
